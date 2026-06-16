59f4b429dbf259edf63b24375abc20f07089f6cb
Vivitoto <1097429376@qq.com>
2026-06-16 09:01:34 +0000

    Release Vanga 0.1.11

--- vanga-app/build.gradle.kts
+++ vanga-app/build.gradle.kts
@@ -111,7 +111,7 @@ android {
         applicationId = "io.github.vivitoto.vanga"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 7
+        versionCode = 8
         versionName = libs.versions.app.version.get()
 
         val enableSelfUpdates = when (androidVariant) {
